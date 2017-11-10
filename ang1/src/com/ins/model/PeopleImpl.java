d02a2627397c8102cb6dfb9678761f5519caf85a
lbs <lbs@nb>
2017-11-09 19:14:39 -0500

    otras

--- ang1/src/com/ins/model/PeopleImpl.java
+++ ang1/src/com/ins/model/PeopleImpl.java
@@ -1,11 +1,7 @@
 package com.ins.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ins.jpa.People;
